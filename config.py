e4b2bf7200e8031d896a54fb3f84b5e7fbccdbfe
Rashid78658 <rashid.kingx01@gmail.com>
2024-09-01 16:25:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7239061539:AAFOnU5ksOmeR4gby1-7WldhyXpOyJqjhJA")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Shakib6667")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6944728729")
 
 
 
